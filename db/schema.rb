61973e70a0b5a58537c1051e3dbd64b289524ae4
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-22 02:49:22 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -31,11 +31,12 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin", default: false
   end
 
 end
