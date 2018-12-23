b9122281c4220bd01b81fc516d8dc64fa18b0ff9
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-23 08:16:56 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,28 +36,3 @@ class UsersController < ApplicationController
     end
 
 end
-
-# describe 'Feature Test: User Signout', :type => :feature do
-
-  # it 'redirects to home page after logging out' do
-  #   visit '/users/new'
-  #   # user_signup method is defined in login_helper.rb
-  #   user_signup
-  #   click_link("Log Out")
-  #   expect(current_path).to eq('/')
-  # end
-
-  # it "successfully destroys session hash when 'Log Out' is clicked" do
-  #   visit '/users/new'
-  #   # user_signup method is defined in login_helper.rb
-  #   user_signup
-  #   click_link("Log Out")
-  #   expect(page.get_rack_session).to_not include("user_id")
-  # end
-  #
-  # it 'has a link to log out from the users/show page when user is an admin' do
-  #   visit '/users/new'
-  #   # admin_signup method is defined in login_helper.rb
-  #   admin_signup
-  #   expect(page).to have_content("Log Out")
-  # end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'users#new'
   resources :users
+  resources :attractions
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  
+
 end
