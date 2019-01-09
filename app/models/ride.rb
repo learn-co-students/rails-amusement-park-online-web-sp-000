5d827db27802f61ed4c493d56a1160fc97cc89c9
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-09 22:39:33 +0200

    write Ride#take_ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,34 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    problem = ""
+    problem += " You do not have enough tickets to ride the #{attraction.name}." unless user_has_enough_tickets?
+    problem += " You are not tall enough to ride the #{attraction.name}." unless user_is_tall_enough?
+    
+    if problem.present?
+      "Sorry." + problem
+    else
+      update_user_for_ride
+    end
+  end
+
+  private
+
+  def user_has_enough_tickets?
+    user.tickets >= attraction.tickets
+  end
+
+  def user_is_tall_enough?
+    user.height >= attraction.min_height
+  end
+
+  def update_user_for_ride
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
+
 end
