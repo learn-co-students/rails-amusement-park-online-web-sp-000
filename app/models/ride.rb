d74a068e14945478a4062f9c8f5e08f0690c4708
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-09 22:43:44 +0200

    refactor take_ride to use problems.any? instead of problems.empty?

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,10 +7,10 @@ class Ride < ActiveRecord::Base
     problems << "You do not have enough tickets to ride the #{attraction.name}." unless user_has_enough_tickets?
     problems << "You are not tall enough to ride the #{attraction.name}." unless user_is_tall_enough?
     
-    if problems.empty?
-      update_user_for_ride
-    else
+    if problems.any?
       ["Sorry.", problems].join(' ')
+    else
+      update_user_for_ride
     end
   end
 
@@ -30,5 +30,4 @@ class Ride < ActiveRecord::Base
     user.happiness += attraction.happiness_rating
     user.save
   end
-
 end
