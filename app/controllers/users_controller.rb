233ccea8be47d7e2ca8bf5ebb386a8debacd1691
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-10 19:32:39 +0200

    add validations for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to @user
     else
-      flash.notice = "Could not create user."
+      flash.notice = ["Could not create user", @user.errors.full_messages].join("\r\n")
       redirect_to new_user_path
     end
     
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:name, :happiness, :nausea, :tickets, :password)
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   validates :name, presence: true
+  validates :height, presence: true, numericality: { only_integer: true }
+  validates :happiness, presence: true, numericality: { only_integer: true }
+  validates :nausea, presence: true, numericality: { only_integer: true }
+  validates :tickets, presence: true, numericality: { only_integer: true }
 
   def mood
     happiness >= nausea ? 'happy' : 'sad'
