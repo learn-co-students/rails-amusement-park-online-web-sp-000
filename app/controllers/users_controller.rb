2a3fdf23f040e3017f44e618dd99fef5cebc05c7
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-10 19:05:15 +0200

    build user#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,20 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to new_user_path, flash[:notice] = "Could not create user."
+    end
+    
+  end
+  
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :happiness, :nausea, :tickets, :password)
   end
   
 end
-
-
-
-
