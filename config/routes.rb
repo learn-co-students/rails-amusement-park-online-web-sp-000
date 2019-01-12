3bff42b155cd57ea6c2d9114382c1b220f42e919
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-12 18:53:42 +0200

    add sign out button to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :attractions
   resources :users
-  get 'signin', to: 'sessions#new', as: :signin_path
+  get 'signin', to: 'sessions#new', as: :signin
   post 'signin', to: 'sessions#create'
-  get 'signout', to: 'sessions#destroy', as: :signout_path
+  get 'signout', to: 'sessions#destroy', as: :signout
   post 'ride/:attraction_id', to: 'rides#create', as: :ride
   # root 'users#new' # should go to some index or something?
   root 'attractions#index'
