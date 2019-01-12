8b2535428feaa84d27977eae2eb35f0056581487
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-12 17:50:18 +0200

    set up ride_path and RidesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :attractions
   resources :users
-  get 'signin', to: 'sessions#new'
+  get 'signin', to: 'sessions#new', as: :signin_path
   post 'signin', to: 'sessions#create'
-  get 'signout', to: 'sessions#destroy'
-  post 'ride', to: 'attractions#ride'
-  root 'users#new'
+  get 'signout', to: 'sessions#destroy', as: :signout_path
+  post 'ride/:id', to: 'rides#create', as: :ride
+  root 'users#new' # should go to some index or something?
 end
