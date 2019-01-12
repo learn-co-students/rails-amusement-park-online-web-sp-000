ff9830715637b2a6be256c0cde4dc6bf325b4e35
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-12 17:51:31 +0200

    include attraction_id in ride route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'signin', to: 'sessions#new', as: :signin_path
   post 'signin', to: 'sessions#create'
   get 'signout', to: 'sessions#destroy', as: :signout_path
-  post 'ride/:id', to: 'rides#create', as: :ride
+  post 'ride/:attraction_id', to: 'rides#create', as: :ride
   root 'users#new' # should go to some index or something?
 end
