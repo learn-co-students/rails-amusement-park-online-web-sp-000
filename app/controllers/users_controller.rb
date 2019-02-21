00bd104183b03d2b3cff03650cd517e4136da0c9
github username <github email address>
2019-02-21 18:02:08 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def signin
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def signout
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  root 'users#new'
+  resources :users
+  get '/signin', to: 'users#new'
 end
