82830d7f0bdfd1abb0338599640b026907160fd9
aburk3 <aburk3@gmail.com>
2019-02-22 05:43:16 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
 #    redirect_to controller: 'welcome', action: 'home'
   end
 
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 
-  
 end
