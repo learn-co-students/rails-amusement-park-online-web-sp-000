9282cab36a7cf44e15ba48f955fc773c5bf2bdc8
aburk3 <aburk3@gmail.com>
2019-02-22 19:16:17 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,14 @@ class SessionsController < ApplicationController
     @user = User.new
     @users = User.all
   end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+    else
+      redirect_to signin_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users
   root "static_pages#home"
   get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 end
