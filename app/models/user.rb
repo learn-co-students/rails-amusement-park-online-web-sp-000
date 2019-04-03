da77719b87209c90427431e50a35206e5e55c2fd
Cookie McCormick <muntananuchat@gmail.com>
2019-04-03 00:36:30 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+  before_save :default_admin
+
+  def default_admin
+    self.admin ||= false
+  end
+
+  def mood
+    if self.happiness && self.nausea
+      mood = self.happiness - self.nausea
+      mood > 0 ? "happy" : "sad"
+    end
+  end
 end
