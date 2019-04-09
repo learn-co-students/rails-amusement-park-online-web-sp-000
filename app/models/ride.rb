f1032d19791d70b1f11029e9685154c6ee73eb0f
Jcg408 <jcg321@comcas.net>
2019-04-08 20:43:55 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,14 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+ 
+    belongs_to :user 
+    belongs_to :attraction
+
+    def take_ride
+        
+        @user = User.find(params[:id])
+        
+        if @user.tickets < Attraction.tickets 
+           return "Sorry. You do not have enough tickets to ride the #{attraction.name}. " 
+        end
+    end
 end
