0e3fdf7763562aae16e93f13ea4ab1fe08cdb147
github username <github email address>
2019-04-22 21:07:13 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :attractions
-  resources :rides
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root 'welcome#home'
+
+   get '/signup', to: 'users#new'
+   post '/signup', to: 'users#create'
+
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   post '/logout', to: 'sessions#destroy'
+
 end
