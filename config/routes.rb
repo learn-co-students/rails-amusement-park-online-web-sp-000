d0e114781e70ad313288b3d910e3e027bc825d9c
Anup Khadka <akhadka.dev@gmail.com>
2019-04-24 21:02:22 -0400

    Incremental Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
+  root 'application#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
