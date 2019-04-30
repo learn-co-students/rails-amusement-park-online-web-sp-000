d6b574c6f2739424aa2851ce19e8d30fc7b14928
github username <github email address>
2019-04-30 14:22:24 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,22 @@
 class RidesController < ApplicationController
+
+  def create
+    @ride = current_user.rides.new(ride_params)
+    result = @ride.take_ride
+    if result == true
+      @ride.save
+      flash[:message] = "Thanks for riding the #{@ride.attraction.name}!"
+    else
+      flash[:message] = result
+    end
+    redirect_to user_path(current_user)
+  end
+
+
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:attraction_id)
+  end
 end
