49e97388bd51f3c92fe53637d22a8772799c051b
Rafael Batista <RafaelB24@yahoo.com>
2019-04-30 21:48:30 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user.rides.build(:attraction_id => params[:attraction_id], :user_id => @user.id ).save if params[:attraction_id]
+
+    redirect_to '/' if !session[:user_id]
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
+  end
+end
