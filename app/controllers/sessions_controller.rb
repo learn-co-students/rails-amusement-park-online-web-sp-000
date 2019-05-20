c7da2e7fb78a3b628403d52b0dfda496974c7251
Btate712 <btate712@gmail.com>
2019-05-19 21:10:36 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+    @users = User.all
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    redirect_to new_session_path unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :attractions
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  root to: 'application#root'
 end
