f3b0bd5f7b54ef394d9c113b888a443930cd5d4d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-07 01:09:42 +0000

    Done.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def iterate_through_attrs(user)
+    signup_attrs = %w[name nausea happiness tickets height]
+    @user.attributes.each do |attr, v|
+      if signup_attrs.include?(attr)
+        yield(attr)
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def init
+    self.admin ||= false
+  end
+
+  def mood
+    self.happiness > self.nausea ? "happy" : "sad"
+  end
 end
