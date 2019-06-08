33258cd896e84054a936c4648f79aa9fd5c852a3
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-08 01:23:56 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,23 +1,15 @@
 class RidesController < ApplicationController
   def create
     attraction = Attraction.find(params[:ride][:attraction_id])
+    ride = Ride.new(ride_params)
+    ride.user_id = session[:user_id]
+    ride.save
 
-    if height_check(attraction) || tickets_check(attraction)
-      binding.pry
-      redirect_to user_path(current_user)
-    else
-      ride = Ride.new(ride_params)
-      ride.user_id = session[:user_id]
-
-      if ride.save
-        ride.update_user
-        binding.pry
-        flash[:alert] << "Thanks for riding the #{attraction.name}!"
-        redirect_to user_path(ride.user)
-      else
-        redirect_to attraction_path(Attraction.find(params[:attraction_id]))
-      end
+    flash[:alert] = ride.take_ride
+    if flash[:alert] == []
+      flash[:alert] = "Thanks for riding the #{attraction.name}!"
     end
+    redirect_to user_path(ride.user)
   end
 
   private
@@ -26,15 +18,5 @@ class RidesController < ApplicationController
     params.require(:ride).permit(:attraction_id)
   end
 
-  def height_check(attraction)
-    if current_user.height < attraction.min_height
-      flash[:alert] << "You are not tall enough to ride the #{attraction.name}."
-    end
-  end
 
-  def tickets_check(attraction)
-    if current_user.tickets < attraction.tickets
-      flash[:alert] << "You do not have enough tickets to ride the #{attraction.name}."
-    end
-  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,6 +2,25 @@ class Ride < ApplicationRecord
   belongs_to :user
   belongs_to :attraction
 
+  def take_ride
+    errors = []
+    if self.user.tickets < self.attraction.tickets
+      errors << "You do not have enough tickets to ride the #{attraction.name}."
+    end
+    if self.user.height < self.attraction.min_height
+      errors << "You are not tall enough to ride the #{attraction.name}."
+    end
+
+    if errors == []
+      update_user
+    else
+      errors.unshift("Sorry.")
+      errors = errors.join(' ')
+    end
+
+    return errors
+  end
+
   def update_user
     self.user.tickets -= self.attraction.tickets
     self.user.nausea += self.attraction.nausea_rating
