54d68085f510ea7bd1e0e7369e88cafbc3f763e2
blainelawson <blaine.lawson@gmail.com>
2019-07-02 15:31:36 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,5 +11,31 @@ class AttractionsController < ApplicationController
     end
 
     def new
+        @attraction = Attraction.new
+    end
+
+    def create
+        if current_user.admin? 
+            @attraction = Attraction.create(attraction_params)
+            redirect_to @attraction
+        end
+    end
+
+    def edit
+        @attraction = Attraction.find_by(id: params[:id])
+    end
+
+    def update
+        if current_user.admin?
+            @attraction = Attraction.find_by(id: params[:id])
+            @attraction.update(attraction_params)
+            redirect_to @attraction
+        end
+    end
+
+    private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
     end
 end
