9fd775de68dbd451578abb9b52c230a3261a7966
Ella <michaella.latkovic@gmail.com>
2019-07-17 23:05:12 -0700

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def index
+  end
+
+  def current_user
+    # User.find(session[:user_id]) || nil
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    session[:user_id]
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if (self.nausea.to_i > self.happiness.to_i)
+      return "sad"
+    elsif (self.nausea.to_i < self.happiness.to_i)
+      return "happy"
+    end
+  end
+
 end
