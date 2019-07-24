b22989a25fd524e0d2be1797c4e0781402522936
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-23 20:10:40 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -9,13 +9,13 @@ class Ride < ApplicationRecord
         
  
         
-                if self.attraction.tickets > self.user.tickets &&  self.attraction.min_height >  self.user.height
-                    "Sorry. You do not have enough tickets to ride the #{ self.attraction.name}. You are not tall enough to ride the #{ self.attraction.name}."
-                elsif  self.attraction.min_height >  self.user.height
-                    "Sorry. You are not tall enough to ride the #{ self.attraction.name}."
-                elsif  self.attraction.tickets >  self.user.tickets
-                    "Sorry. You do not have enough tickets to ride the #{ self.attraction.name}."
-                else
+                if !enough_tickets? &&  !min_height?
+                    "Sorry. " + not_enough_tickets_msg + " " + not_tall_enough_msg 
+                elsif  !min_height?
+                    "Sorry. " + not_tall_enough_msg 
+                elsif  !enough_tickets?
+                    "Sorry. " + not_enough_tickets_msg
+                 else
                     self.user.tickets -=  self.attraction.tickets
                     self.user.nausea +=  self.attraction.nausea_rating
                     self.user.happiness +=  self.attraction.happiness_rating
@@ -24,6 +24,19 @@ class Ride < ApplicationRecord
                 end
     end 
 
-     
+    def enough_tickets?
+        self.user.tickets >= self.attraction.tickets 
+    end 
 
+    def min_height?
+        self.user.height >=self.attraction.min_height 
+    end 
+
+    def not_tall_enough_msg
+        "You are not tall enough to ride the #{ self.attraction.name}."
+    end 
+
+    def not_enough_tickets_msg
+        "You do not have enough tickets to ride the #{ self.attraction.name}."
+    end 
 end
