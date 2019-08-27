862d48432c2c6ec0ff4c8ec19c73d345008ff695
Alan Krajina <alankrajina88@gmail.com>
2019-08-27 10:23:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :rides
   resources :attractions
   resources :users
+
+  root 'welcome#hello'
+  get '/signin', to: 'users#new'
+  post '/signin', to: 'users#create'
+#  delete '/signout', to: 'sessions#destroy'
+
+#  get    '/login',   to: 'sessions#new'
+#  post   '/login',   to: 'sessions#create
 end
