d702fecbebc2d6dce0965e94ad4b1f052bed9385
Alan Krajina <alankrajina88@gmail.com>
2019-08-27 10:10:43 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  validates :password, presence: true
+
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
 end
