976c06e8317d4bfb10c51033e04490d0b36fed46
Alan Krajina <alankrajina88@gmail.com>
2019-08-27 10:32:15 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
+  include UsersHelper
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
   end
+
+  def create
+        @user = User.new(user_params)
+        if @user.save
+          redirect_to @user
+        else
+            redirect_to '/'
+        end
+    end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
+    end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  def mood(user)
+#    binding.pry
+    if user.nausea > user.happiness
+      user.happiness = "sad"
+    elsif  user.nausea < user.happiness
+      user.happiness = "happy"
+    end
+  end
+
 end
