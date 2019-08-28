e5cdda7c0e1b89fe0fbea5fce7545ea01bf7e9d2
Andrew Davisson <akdavisson4@gmail.com>
2019-08-27 20:38:39 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:user][:password] != params[:user][:password_confirmation]
+      render :new
+    end
+
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :happiness, :nausea, :height, :tickets, :admin)
+  end
+
+end
