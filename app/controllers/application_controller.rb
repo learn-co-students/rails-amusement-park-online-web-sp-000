f73b77148832af7ad114c7590ee39a0436411121
Andrew Davisson <akdavisson4@gmail.com>
2019-08-30 10:12:23 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def require_login
     redirect_to root_url unless session.include? :user_id
   end
+
+  def current_user
+    if session.include? :user_id
+      User.find(session[:user_id])
+    end
+  end
 end
--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,6 +8,30 @@ class AttractionsController < ApplicationController
   end
 
   def create
+    user = current_user
+    attraction = user.attractions.build(attractions_params)
+    attraction.save
+    redirect_to attraction_path(attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attractions_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+  end
+
+  private
+
+  def attractions_params
+    params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
   end
 
 end
