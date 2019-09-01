6d1ab031b6f7d3da891dc9b74ccab28787b1a0b2
Ramon Chavez <rachavez1974@gmail.com>
2019-08-31 22:16:49 -0400

    Finish rails-amusement-park-online-web-sp-000

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,4 +1,15 @@
 class AttractionsController < ApplicationController
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params(params[:attraction].keys))
+    if @attraction.save
+      redirect_to @attraction
+    end
+  end
+
   def index
     @attractions = Attraction.all
   end
@@ -6,4 +17,21 @@ class AttractionsController < ApplicationController
   def show
     @attraction = Attraction.find(params[:id])
   end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update_attributes(attraction_params(params[:attraction].keys))
+      redirect_to @attraction
+    end
+  end
+
+  private
+    def attraction_params(*args)
+      params.require(:attraction).permit(*args)
+      
+    end
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,17 @@
 class RidesController < ApplicationController
+
+  def create
+    @ride = Ride.new(user_id: current_user.id, attraction_id: params[:attraction])
+    ride_allowed = @ride.take_ride
+
+    if ride_allowed == true
+      if @ride.save
+        flash[:notice] = "Thanks for riding the #{@ride.attraction.name}!"
+        redirect_to @ride.user
+      end
+    else
+      flash[:notice] = ride_allowed
+      redirect_to @ride.user
+    end  
+  end
 end
