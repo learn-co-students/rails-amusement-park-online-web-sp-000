a267db4db32430345b5dbfd204b43e16e91e5089
Adil Naimi <adilnm23@hotmail.com>
2019-10-15 19:15:59 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+   # before_action :require_login
+   # skip_before_action :require_login, only: [:new]
   def new
     @user=User.new
   end
@@ -17,6 +18,8 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only:[:new, :create, :show]
+  resources :users
+  resources :attractions
   get 'signin' => "sessions#new"
   post 'sessions' => "sessions#create"
+  get 'signout' => "sessions#destroy"
   root 'users#new'
 
 end
