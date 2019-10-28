46862641b70ad56bd3dd7f44b0cfc079be41a793
Meisuleen Kased <meisuleen@gmail.com>
2019-10-28 22:32:51 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class RidesController < ApplicationController
   before_action :require_login
 
@@ -5,8 +6,7 @@ class RidesController < ApplicationController
     ride = Ride.create(user_id: current_user.id, attraction_id: params[:attraction_id])
     if ride
       response = ride.take_ride
-      flash[:notice] = response
-      redirect_to user_path(ride.user)
+      redirect_to user_path(ride.user), notice: response
     else
       redirect_to attraction_path(ride.attraction)
     end
