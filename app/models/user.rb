629a6baf11ce2b3551548e227819716513056a01
jgrantps <jgrantps@hotmail.com>
2019-10-31 09:29:29 -0300

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, :presence => { :on => :create }, confirmation: true
+  validates :name, :happiness, :nausea, :height, :tickets, presence: true
+
+  has_many :rides
+  has_many :attractions, through: :rides
 end
