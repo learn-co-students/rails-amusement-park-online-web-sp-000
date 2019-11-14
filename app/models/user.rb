602ae1c42453a6ad6c94e55c7f2989a5199bc4c5
Paul Rosero <paul_rosero@yahoo.com>
2019-11-14 13:40:59 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :rides
-    has_many :attractions, through: :rides
-  
-    def admin
-        false
-    end
+  has_many :rides
+  has_many :attractions, :through => :rides
 
-    def mood
-        if self.nausea > self.happiness
-            "sad"
-        else
-            "happy"
-        end
+  def mood
+    unless admin
+      happiness > nausea ? "happy" : "sad"
     end
+  end
 end
