d07e56e49a9460910668b43ade5c19540472f535
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-14 19:04:22 -0800

    models passed

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,35 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    user = User.find_by_id(self.user_id)
+    attraction = Attraction.find_by_id(self.attraction_id)
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      tickets_left = user.tickets - attraction.tickets
+      nausea_level = user.nausea + attraction.nausea_rating
+      happiness_level = user.happiness + attraction.happiness_rating
+      # must use update_attribute
+      user.update_attribute(:tickets, tickets_left)
+      user.update_attribute(:nausea, nausea_level)
+      user.update_attribute(:happiness, happiness_level)
+    end
+  end
+
+
+
 end
+
+
+  # has a method 'take_ride' that accounts for the user not having enough tickets (FAILED - 45)
+  # has a method 'take_ride' that accounts for the user not being tall enough (FAILED - 46)
+  # has a method 'take_ride' that accounts for the user not being tall enough and not having enough tickets (FAILED - 47)
+  # has a method 'take_ride' that updates ticket number (FAILED - 48)
+  # has a method 'take_ride' that updates the user's nausea (FAILED - 49)
+  # has a method 'take_ride' that updates the user's happiness (FAILED - 50)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  [:name, :password].each do |attribute|
+    validates(attribute, presence: true)
+  end
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness > self.nausea
+      return "happy"
+    else
+     return "sad"
+   end
+  end
+
 end
+
+#:happiness, :nausea, :height, :tickets #=> the additional validations causes the test to fail.
