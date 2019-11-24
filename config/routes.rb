69f26cd709eb26f24c79df79571c93e969963500
Dakota Clark <dakotaclark92@yahoo.com>
2019-11-24 02:31:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  root "welcome#index"
+    resources :users
+    resources :attractions
+
+    get '/signin' => "sessions#new"
+    post '/signin' => "sessions#create"
+    delete '/signout' => "sessions#sign_out"
+
+     post '/ride' => "rides#new"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
