d8daf1eae8617f51e5b24a4380175a857dda5cfe
tomciv9000 <eugene.white@gmail.com>
2020-01-05 22:18:57 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    validates :name, presence: true
+    validates :password_digest, presence: true
+  
+    def mood
+        if self.nausea != nil && self.happiness !=nil
+            self.happiness > self.nausea ? "happy" : "sad"
+        end
+    end
+
 end
