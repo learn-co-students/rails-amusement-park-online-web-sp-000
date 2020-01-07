c160378b94bc69aaf3fb18b8b721d2ea960de4b6
jei-doubleton <jennifer.singleton@gmail.com>
2020-01-06 18:54:27 -0600

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,47 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        return "Sorry. You do not have enough tickets to ride the #{coaster.name}. You are not tall enough to ride the #{coaster.name}." if !tall_enough && !enough_tickets
+
+        return "Sorry. You do not have enough tickets to ride the Roller Coaster." unless enough_tickets
+        
+        return "Sorry. You are not tall enough to ride the #{coaster.name}." unless tall_enough
+
+        update_tickets
+        update_nausea
+        update_happiness
+    end
+
+    def enough_tickets
+        true if rider.tickets >= coaster.tickets
+    end
+
+    def tall_enough
+        true if rider.height >= coaster.min_height
+    end
+
+    def update_tickets
+        rider.tickets -= coaster.tickets
+        rider.save
+    end
+
+    def update_nausea
+        rider.nausea += coaster.nausea_rating
+        rider.save
+    end
+
+    def update_happiness
+        rider.happiness += coaster.happiness_rating
+        rider.save
+    end
+
+    def rider 
+        self.user
+    end
+
+    def coaster
+        self.attraction
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    has_many :rides
+    has_many :attractions, through: :rides
+    validates_presence_of :password
+
+    def mood
+        if self.happiness > self.nausea
+            return "happy"
+        elsif self.nausea > self.happiness
+            return "sad"
+        end
+    end
+
 end
