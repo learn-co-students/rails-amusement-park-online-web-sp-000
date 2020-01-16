d01ef4363d74f1a7f3f0e7f9269c6b4537096b90
Frederic Silber <freddy.silber@cloudcoach.com>
2020-01-16 05:34:41 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#home"
-  resources :users, only: [:new, :create, :show]
+  get '/', to: 'users#home', as: '/root'
+
+  get '/signin', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  post '/sessions', to: 'sessions#create'
+  resources :users
   resources :attractions
-  get '/signin', to: 'session#new'
-  post '/signin', to: 'session#create'
-  delete '/session', to: 'session#destroy'
-  post '/rides', to: 'rides#create'
+  resources :rides, only: [:create]
 end
