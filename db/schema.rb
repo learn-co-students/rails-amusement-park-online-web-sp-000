bce649ca28a6edb404a32afc1f460696b9e763b8
Frederic Silber <freddy.silber@cloudcoach.com>
2020-01-16 05:12:07 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_01_16_042306) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
+    t.string "password"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "tickets"
