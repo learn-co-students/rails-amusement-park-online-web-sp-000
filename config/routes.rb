2c7b3882f187c63ec4908dfe0df636cf3ae5131c
Educhin <c.s.arinze@gmail.com>
2020-02-11 16:27:30 -0500

    Add show view to attractions and method to controller. Allow show route in attractions resources.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :attractions, only: [:index]
+  resources :attractions, only: [:index, :show]
 end
