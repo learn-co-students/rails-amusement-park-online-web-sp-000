f9ef695a11280acc2f008571ca7696aff7e62024
Educhin <c.s.arinze@gmail.com>
2020-02-11 22:50:21 -0500

    Add to helper methods to attractions helper to show links on index page contigent upon admin status. Display methods in index view. Create new and edit pages for attractions.

--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -1,2 +1,21 @@
 module AttractionsHelper
+    def new_attraction_link
+        if session[:user_id]
+            user = User.find(session[:user_id])
+            if user.admin
+                link_to "New Attraction", "some_link"
+            end
+        end
+    end
+
+    def show_attraction_link(attraction)
+        if session[:user_id]
+            user = User.find(session[:user_id])
+            if user.admin
+                link_to "Show #{attraction.name}", attraction_path(attraction)
+            else
+                link_to "Go on #{attraction.name}", attraction_path(attraction)
+            end
+        end
+    end
 end
