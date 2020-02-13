5f9ec7103a43cc629eca662c589494f10749e471
juan siu <juan.siu@gmail.com>
2020-02-13 03:04:06 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
-    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+
+    @user = User.find_by(name: params[:name])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    #log_in(@user)
     session[:user_id] = @user.id
+    redirect_to @user
   end
 
   def destroy
+    #log_out
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,17 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      #log_in(@user)
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to @user
     else
-      redirect_to controller: 'users', action: 'new'
+      render 'new'
     end
   end
 
