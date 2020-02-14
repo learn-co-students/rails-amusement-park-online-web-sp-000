e24eb3d31c10af4cec73bd644502a3e8782c1c80
Lindsey Wells <github email address>
2020-02-14 13:02:02 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_122217) do
+ActiveRecord::Schema.define(version: 2020_02_14_130112) do
+
+  create_table "rides", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "attraction_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
