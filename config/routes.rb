741335fa040f57d07a946be9fe0fc7e8d323e145
Blythe Pollard <blytheepollard@gmail.com>
2020-02-14 16:25:43 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/signin' => 'users#new'
   post '/signin' => 'users#create'
 
+  get '/admin', to: 'admin#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
