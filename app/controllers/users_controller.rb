ac1471c212923284527e91ecedd7a65c78381eb1
Lindsey Wells <github email address>
2020-02-16 13:05:36 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    redirect_to users_path(@user)
+    @user = User.create(user_params)
+    session[:user_id]=@user.id
+    redirect_to user_path(@user.id)
   end
+
+  def show
+    @user= User.find(params[:id])
+  end
+
 
   private
 
