c27f0577858fc08063858c91ae9d19169ff23310
Lindsey Wells <github email address>
2020-02-16 18:45:36 +0000

    Done.

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,13 @@
+class SessionController < ApplicationController
+
+  def create
+    user = User.find(params[:user][:user_id])
+    if user.authenticate(params[:password])
+      session[:user_id] = params[:user_id]
+      redirect_to user_path(user)
+    else
+      render 'users/signin'
+    end
+  end
+
+  end
