2a0bbade672e3239565bf3e9a381044d008e68cb
Lindsey Wells <github email address>
2020-02-18 20:33:24 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,4 +11,20 @@ end
     @attraction = Attraction.find(params[:id])
   end
 
+  def new
+    @attraction=Attraction.new
+  end
+
+  def create
+    @attraction=Attraction.create(attraction_params)
+    @attraction.save
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+  end
+
 end
