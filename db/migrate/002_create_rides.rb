5b60c26cb70051ec66b6148fd5f057815fe53393
Bernadette Davis <bsd231@nyu.edu>
2020-02-18 01:32:10 +0000

    Done.

--- db/migrate/002_create_rides.rb
+++ db/migrate/002_create_rides.rb
@@ -1,13 +1,12 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class CreateRides < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :rides do |t|
       t.string :name
-      t.string :password_digest
-      t.integer :nausea
-      t.integer :happiness
       t.integer :tickets
-      t.integer :height
-
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+      
       t.timestamps
     end
   end
--- db/migrate/003_create_attractions.rb
+++ db/migrate/003_create_attractions.rb
@@ -1,12 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[5.2]
+class CreateAttractions < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :attractions do |t|
       t.string :name
-      t.string :password_digest
-      t.integer :nausea
-      t.integer :happiness
       t.integer :tickets
-      t.integer :height
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
 
       t.timestamps
     end
