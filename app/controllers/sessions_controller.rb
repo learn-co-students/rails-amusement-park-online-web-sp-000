f8ed76ec8912d615b8447c5bbefef44a8ec06801
indre93 <indre.bakeer@gmail.com>
2020-03-31 14:11:34 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,20 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.user_id
+      
+      redirect_to user_path(@user)
+    else
+      redirect_to signin_path
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
   end
 end
