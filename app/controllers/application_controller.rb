ffd9050b09966f7ab2b0d0534db35cb7095a83db
indre93 <indre.bakeer@gmail.com>
2020-03-31 20:36:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
   end
 
   def current_user
-    @user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   helper_method :current_user
