a9cef29c3d4e8ef2049147ef3071cb7ed240747b
indre93 <indre.bakeer@gmail.com>
2020-04-01 00:38:09 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,24 +3,24 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-   
+    user = self.user
 
-    if @attraction.tickets > @user.tickets && @attraction.min_height > @user.height
-      "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
-    elsif @attraction.tickets > @user.tickets
-      "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
-    elsif @attraction.min_height > @user.height
-      "Sorry. You are not tall enough to ride the #{@attraction.name}."
+    if attraction.tickets > user.tickets && attraction.min_height > user.height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif attraction.tickets > user.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif attraction.min_height > user.height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
       update_user
     end
   end
 
   def update_user
-    @user[:tickets] = @user.tickets - @attraction.tickets
-    @user[:nausea] = @user.nausea + @attraction.nausea_rating
-    @user[:happiness] = @user.happiness + @attraction.happiness_rating
-    @user.save
+    user[:tickets] = user.tickets - attraction.tickets
+    user[:nausea] = user.nausea + attraction.nausea_rating
+    user[:happiness] = user.happiness + attraction.happiness_rating
+    user.save
   end
   
 end
