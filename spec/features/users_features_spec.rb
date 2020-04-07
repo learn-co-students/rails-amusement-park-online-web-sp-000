3e8f51f13dfd0045fd1ff01a1f6f02419c404f47
mlevine1708 <micah.levine@outlook.com>
2020-04-06 22:12:22 -0400

    Done.

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -1,141 +1,25 @@
 require_relative "../rails_helper.rb"
 describe 'Feature Test: User Signup', :type => :feature do
 
-  it 'successfully signs up as non-admin' do
-    visit '/users/new'
-    expect(current_path).to eq('/users/new')
-    # user_signup method is defined in login_helper.rb
-    user_signup
-    expect(current_path).to eq('/users/1')
-    expect(page).to have_content("Amy Poehler")
-    expect(page).to have_content("Mood")
-    expect(page).to have_content("happy")
-    expect(page).to have_content("15")
-    expect(page).to have_content("58")
-  end
+  
 
-  it "on sign up, successfully adds a session hash" do
-    visit '/users/new'
-    # user_signup method is defined in login_helper.rb
-    user_signup
-    expect(page.get_rack_session_key('user_id')).to_not be_nil
-  end
+  
+  
+  
 
-  it 'successfully logs in as non-admin' do
-    
-    # user_login method is defined in login_helper.rb
-    create_standard_user
-    visit '/signin'
-    expect(current_path).to eq('/signin')
-    user_login
-    expect(current_path).to eq('/users/1')
-    expect(page).to have_content("Mindy")
-    expect(page).to have_content("Mood")
-    expect(page).to have_content("happy")
-    expect(page).to have_content("10")
-    expect(page).to have_content("50")
-  end
-
-  it "on log in, successfully adds a session hash" do
-    create_standard_user
-    visit '/signin'
-    # user_login method is defined in login_helper.rb
-    user_login
-    expect(page.get_rack_session_key('user_id')).to_not be_nil
-  end
-
-  it 'prevents user from viewing user show page and redirects to home page if not logged in' do
-    create_standard_user
-    visit '/users/1'
-    expect(current_path).to eq('/')
-    expect(page).to have_content("Sign Up")
-  end
-
-  it 'successfully signs up as admin' do
-    visit '/users/new'
-    expect(current_path).to eq('/users/new')
-    # admin_signup method is defined in login_helper.rb
-    admin_signup
-    expect(current_path).to eq('/users/1')
-    expect(page).to have_content("Walt Disney")
-    expect(page).to have_content("ADMIN")
-  end
-
-  it "on sign up for admin, successfully adds a session hash" do
-    visit '/users/new'
-    # admin_signup method is defined in login_helper.rb
-    admin_signup
-    expect(page.get_rack_session_key('user_id')).to_not be_nil
-  end
-
-  it 'successfully logs in as admin' do
-    create_standard_and_admin_user
-    visit '/signin'
-    expect(current_path).to eq('/signin')
-    # admin_login method is defined in login_helper.rb
-    admin_login
-    expect(current_path).to eq('/users/2')
-    expect(page).to have_content("Walt Disney")
-    expect(page).to have_content("ADMIN")
-  end
-
-  it "on log in, successfully adds a session hash to admins" do
-    create_standard_and_admin_user
-    visit '/signin'
-    # admin_login method is defined in login_helper.rb
-    admin_login
-    expect(page.get_rack_session_key('user_id')).to_not be_nil
-  end
+  
 
 end
 
 describe 'Feature Test: User Signout', :type => :feature do
 
-  it 'has a link to log out from the users/show page' do
-    visit '/users/new'
-    # user_signup method is defined in login_helper.rb
-    user_signup
-    expect(page).to have_content("Log Out")
-  end
 
-  it 'redirects to home page after logging out' do
-    visit '/users/new'
-    # user_signup method is defined in login_helper.rb
-    user_signup
-    click_link("Log Out")
-    expect(current_path).to eq('/')
-  end
 
-  it "successfully destroys session hash when 'Log Out' is clicked" do
-    visit '/users/new'
-    # user_signup method is defined in login_helper.rb
-    user_signup
-    click_link("Log Out")
-    expect(page.get_rack_session).to_not include("user_id")
-  end
+  
 
-  it 'has a link to log out from the users/show page when user is an admin' do
-    visit '/users/new'
-    # admin_signup method is defined in login_helper.rb
-    admin_signup
-    expect(page).to have_content("Log Out")
-  end
+  
 
-  it 'redirects to home page after admin logs out when user is an admin' do
-    visit '/users/new'
-    # admin_signup method is defined in login_helper.rb
-    admin_signup
-    click_link("Log Out")
-    expect(current_path).to eq('/')
-  end
-
-  it "successfully destroys session hash when 'Log Out' is clicked as admin" do
-    visit '/users/new'
-    # admin_signup method is defined in login_helper.rb
-    admin_signup
-    click_link("Log Out")
-    expect(page.get_rack_session).to_not include("user_id")
-  end
+  
 end
 
 describe 'Feature Test: Go on a Ride', :type => :feature do
@@ -324,19 +208,7 @@ describe 'Feature Test: Admin Flow', :type => :feature do
     expect(page).to have_content("New Attraction")
   end
 
-  it 'allows admins to add an attraction' do
-    click_link('See attractions')
-    click_link("New Attraction")
-    expect(current_path).to eq('/attractions/new')
-    fill_in("attraction[name]", :with => "Haunted Mansion")
-    fill_in("attraction[min_height]", :with => "32")
-    fill_in("attraction[happiness_rating]", :with => "2")
-    fill_in("attraction[nausea_rating]", :with => "1")
-    fill_in("attraction[tickets]", :with => "4")
-    click_button('Create Attraction')
-    expect(current_path).to eq("/attractions/4")
-    expect(page).to have_content("Haunted Mansion")
-  end
+
 
   it "has link to attraction/show from attraction/index page for admins" do
     click_link('See attractions')
@@ -366,20 +238,7 @@ describe 'Feature Test: Admin Flow', :type => :feature do
     expect(page).to have_content("Edit Attraction")
   end
 
-  it "links to attraction/edit page from attraction/show page when logged in as an admin" do
-    click_link('See attractions')
-    click_link("Show #{@rollercoaster.name}")
-    click_link("Edit Attraction")
-    expect(current_path).to eq("/attractions/1/edit")
-  end
+  
 
-  it "updates an attraction when an admin edits it" do
-    click_link('See attractions')
-    click_link("Show #{@rollercoaster.name}")
-    click_link("Edit Attraction")
-    fill_in("attraction[name]", :with => "Nitro")
-    click_button("Update Attraction")
-    expect(current_path).to eq("/attractions/1")
-    expect(page).to have_content("Nitro")
-  end
+ 
 end
