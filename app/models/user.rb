818d97242ba1599efbffa5aa2a314f5b8eea04c4
brianpompey <brianthescholar@gmail.com>
2020-04-14 11:14:43 -0400

    models done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+#    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    validates :password, presence: true
+
+
+    def mood
+        unless self.admin
+            if self.happiness > self.nausea
+                "happy"
+            else
+                "sad"    
+            end
+        end
+    end
 end
