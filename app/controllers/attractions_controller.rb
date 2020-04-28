03353ce2cb051f62de67fa36caee23fb988c704c
lukesherwood <luke.sherwood55@gmail.com>
2020-04-29 10:17:12 +1200

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,30 @@ class AttractionsController < ApplicationController
         @ride = Ride.new
         @attraction = Attraction.find(params[:id])
     end
+
+    def new
+        @attraction = Attraction.new
+    end
+
+    def create
+        @attraction = Attraction.create(attraction_params)
+        return redirect_to new_attraction_path unless @attraction.save
+        redirect_to attraction_path(@attraction)
+    end
+
+    def edit
+        @attraction = Attraction.find(params[:id])
+    end
+
+    def update
+        @attraction = Attraction.find(params[:id])
+        return redirect_to edit_attraction_path unless @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end
+
+    private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+    end
 end
