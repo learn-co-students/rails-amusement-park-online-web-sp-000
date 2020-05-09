8d541ac5bd10010d0d939f89f1bc060124cc82e4
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-09 11:38:05 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,18 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        if self.attraction.tickets > self.user.tickets && self.user.height < self.attraction.min_height
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+        elsif self.attraction.tickets > self.user.tickets 
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+        elsif self.user.height < self.attraction.min_height
+            "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+        else
+            self.user.update(tickets: (self.user.tickets - self.attraction.tickets), nausea: (self.user.nausea + self.attraction.nausea_rating), happiness: (self.user.happiness + self.attraction.happiness_rating))
+            "Thanks for riding the #{attraction.name}!"
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  resources :users
+  resources :rides
+  resources :attractions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
