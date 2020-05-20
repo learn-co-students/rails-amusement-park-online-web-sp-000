e4689937f2343e8915dfc2da52d224d650133644
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-20 16:33:22 -0400

    rides basic setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "nausea_rating"
     t.integer "happiness_rating"
     t.integer "min_height"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
   end
 
   create_table "users", force: :cascade do |t|
