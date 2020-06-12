3c2f1916c70e800c5449cbb866dd69f714696d01
ottocedeno <otto@creativespaceclub.com>
2020-06-11 23:14:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#index'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create', as: 'submit_signin'
+  match '/logout' => 'sessions#destroy', via: [:get, :post]
+
+  resources :users, only: [:new, :create, :show]
+  resources :attractions, only: [:index, :show, :new, :create, :edit, :update]
+  resources :rides, only: [:create]
 end
