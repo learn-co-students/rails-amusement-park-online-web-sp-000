7a45de3ec0390be531d393a4542f1423b8036882
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-06 19:33:16 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/', to: 'users#new'
   get '/logout', to: 'users#destroy'
   get '/attractions', to: 'attractions#index'
+  post '/rides', to:"rides#create", as: 'rides'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
