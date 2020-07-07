133b3b308175f0bf3d4bdf526427f7e11bb87c5e
blee2125 <blee2125@gmail.com>
2020-07-07 12:15:47 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+    attribute :admin, :boolean, default: "false"
+
+    
+
+    def mood
+        if !admin
+            happiness > nausea ? "happy" : "sad"
+
+        end
+    end
 end
