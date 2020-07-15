1c0e19fe7fe557266171be65d5d485f9a9670895
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-07-15 19:36:00 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+
+class UsersController < ApplicationController
+
+  def welcome
+    redirect_to controller: 'users', action: 'new' unless session[:password]
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'users', action: 'welcome'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+  
+end
