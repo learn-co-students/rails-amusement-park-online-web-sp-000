5e2e9142ba7342179a752933e46851db9d2d66d1
CoderJay06 <jaycru06@yahoo.com>
2020-07-17 02:29:54 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,18 @@
 class Ride < ActiveRecord::Base
    belongs_to :attraction
    belongs_to :user
+
+   def take_ride 
+      if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+         "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+      elsif self.user.tickets < self.attraction.tickets
+         "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+      elsif self.user.height < self.attraction.min_height 
+         "Sorry. You are not tall enough to ride the #{attraction.name}." 
+      elsif self.user.tickets >= self.attraction.tickets
+         self.user.update(:tickets => self.user.tickets - self.attraction.tickets,
+                          :nausea => self.user.nausea + self.attraction.nausea_rating,
+                          :happiness => self.user.happiness + self.attraction.happiness_rating)
+      end 
+   end 
 end
--- db/migrate/20200717052502_add_integer_columns_to_users.rb
+++ db/migrate/20200717052502_add_integer_columns_to_users.rb
@@ -1,8 +1,4 @@
 class AddIntegerColumnsToUsers < ActiveRecord::Migration[5.2]
-  # :nausea => 5,
-  # :happiness => 3,
-  # :tickets => 4,
-  # :height => 34
   def change
     add_column :users, :nausea, :integer 
     add_column :users, :happiness, :integer 
