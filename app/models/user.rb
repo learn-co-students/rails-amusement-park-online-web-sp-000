6239db1126a92604d754c2225084cf5d21b96afd
CoderJay06 <jaycru06@yahoo.com>
2020-07-18 00:20:40 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
    has_many :rides
    has_many :attractions, through: :rides 
+   has_secure_password 
+
+   def mood 
+      # returns 'sad' when the user is more nauseous than happy
+      if self.nausea > self.happiness 
+         "sad"
+      else  
+         "happy"
+      end 
+   end 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_052502) do
+ActiveRecord::Schema.define(version: 2020_07_18_041452) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 2020_07_17_052502) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.integer "nausea"
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin", default: false
   end
 
 end
