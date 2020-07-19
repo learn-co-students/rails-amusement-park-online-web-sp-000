7cc21d68a9b2a81542da3730b4222b0d89e9556a
carrollm2 <carrollm2@gmail.com>
2020-07-19 12:32:53 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,15 @@ class UsersController < ApplicationController
 
     def create
 
-        if params[:user][:name].present? && params[:user][:password].present?
+        if params[:user][:name].present? && params[:user][:password].present? && params[:user][:admin] == "1"
+            @user = User.create(name: params[:user][:name], password: params[:user][:password], admin: params[:user][:admin])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)             
+        elsif params[:user][:name].present? && params[:user][:password].present?
             @user = User.create(user_params)
             session[:user_id] = @user.id 
             redirect_to user_path(@user)    
-          else
+        else
             redirect_to controller: 'users', action: 'new'
         end      
     end
