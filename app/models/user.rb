58d01fc72348abc0e5cdadca4f5bc4852b6ccebc
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-22 19:33:18 -0700

    add mood method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides 
 
   has_secure_password
+
+  def mood 
+    # if happiness greater than or equal to nausea = true return 'happy' else return 'sad'
+    self.happiness >= self.nausea ? 'happy' : 'sad'
+  end
+
 end
