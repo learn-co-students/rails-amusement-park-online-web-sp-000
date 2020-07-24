42205ceefcb9984bc825d2fb67d308d8143b31b6
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-23 21:45:48 -0700

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -5,9 +5,14 @@ class RidesController < ApplicationController
   end 
 
   def create
-    ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    msg = ride.take_ride
-    redirect_to user_path(ride.user)
-  end 
+    ride = Ride.create(user_id: current_user.id, attraction_id: params[:attraction_id])
+    if ride
+      response = ride.take_ride
+      flash[:notice] = response
+      redirect_to user_path(ride.user)
+    else
+      redirect_to attraction_path(ride.attraction)
+    end
+  end
   
 end
