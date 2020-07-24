c2a6a4989c36d13a8ebd192bb64afed995a0f4c7
carrollm2 <carrollm2@gmail.com>
2020-07-23 21:20:04 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  add_flash_types :thanks, :tickets, :height
+
 end
--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,7 +7,18 @@ class AttractionsController < ApplicationController
         @user = User.find(session[:user_id])
         @attraction = Attraction.find(params[:id])
         ride = Ride.create(user_id: @user.id, attraction_id: params[:id])
-        ride.take_ride                
+        ride.take_ride  
+
+        if @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
+            flash[:tickets] = "You do not have enough tickets to ride the #{@attraction.name}" 
+            flash[:height] = "You are not tall enough to ride the #{@attraction.name}" 
+        elsif @user.height < @attraction.min_height
+            flash[:height] = "You are not tall enough to ride the #{@attraction.name}"
+        elsif @user.tickets < @attraction.tickets    
+            flash[:tickets] = "You do not have enough tickets to ride the #{@attraction.name}"
+        else
+            flash[:thanks] = "Thanks for riding the #{@attraction.name}!"    
+        end          
     end
 
 end
