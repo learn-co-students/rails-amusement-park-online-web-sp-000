a189f23f2c800365831b5f4e50f323ed7dd2d1ac
carrollm2 <carrollm2@gmail.com>
2020-07-23 23:23:43 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -45,6 +45,9 @@ class AttractionsController < ApplicationController
     end
 
     def update
+        @attraction = Attraction.find(params[:id])
+        @attraction.update(name: params[:attraction][:name], tickets: params[:attraction][:tickets], nausea_rating: params[:attraction][:nausea_rating], happiness_rating: params[:attraction][:happiness_rating], min_height: params[:attraction][:min_height])
+        redirect_to attraction_path(@attraction)
     end
 
 end
