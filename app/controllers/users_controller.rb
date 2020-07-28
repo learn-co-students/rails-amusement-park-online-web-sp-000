373f370643cbdf0c94cfd0dd21bb4d8bf1e4db8b
wkdewey <wkdewey@gmail.com>
2020-07-28 18:41:59 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    if logged_in?
+      @user = User.find(params[:id])
+    else
+      flash[:error] = "Please Sign Up"
+      redirect_to root_path
+    end
+  end
+
+  def update
+    ride = Ride.create(user_id: current_user.id, attraction_id: session[:last_attraction])
+    flash[:message] = ride.take_ride
+    redirect_to user_path(current_user)
+  end
+
+  def signin
+    @user = User.new
+    @users = User.all
+  end
+
+  def logout
+    session.destroy
+    redirect_to root_path
+  end
+  
+  def sign_user_in
+    @user = User.find_by(name: user_params[:name])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :nausea, :happiness, :tickets, :admin)
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+  after_initialize :init
+
+  def init
+    self.admin ||= false
+  end
+
+  def mood
+    if self.nausea && self.happiness
+      if self.nausea > self.happiness
+        "sad"
+      else
+        "happy"
+      end
+    end
+  end
 end
