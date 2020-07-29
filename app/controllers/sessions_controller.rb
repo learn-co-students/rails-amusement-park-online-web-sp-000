7bd6f321cf42d691482eca0d356bc067b3ef9000
github username <github email address>
2020-07-28 20:49:23 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+   def new
+      
+   end 
+
+   def create 
+      # byebug
+      @user = User.find_by(params[:name])
+      if @user && @user.authenticate(params[:password])
+         session[:user_id] = @user.id 
+         redirect_to user_path(@user)
+      else 
+         render 'new'
+      end  
+   end 
+
+   def destroy 
+   end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions
+  get "/signin", to: 'sessions#new'
+  post "/signin", to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "users#new"
 end
