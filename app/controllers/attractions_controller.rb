f6085ec452801ae2c703ce016e249168b0b02554
CoderJay06 <jaycru06@yahoo.com>
2020-08-01 04:17:21 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -13,6 +13,9 @@ class AttractionsController < ApplicationController
       attraction = Attraction.find(params[:id])
       ride = Ride.create(user_id: current_user.id, attraction_id: attraction.id)
       ride.take_ride
+      if !ride.user_height_not_tall_enough? && ride.user_tickets_are_enough
+         flash[:notice] = "Thanks for riding the #{attraction.name}!"
+      end 
       # current_user.tickets -= attraction.tickets 
       # current_user.save
       redirect_to user_path(current_user)
