a963c27fe576ca7e3b0fa4ba457ff6d9a6987480
CoderJay06 <jaycru06@yahoo.com>
2020-08-01 04:02:09 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -10,9 +10,11 @@ class AttractionsController < ApplicationController
 
    def user_goes_on_ride
       # byebug
-      attraction = Attraction.find_by(params[:name])
-      current_user.tickets -= attraction.tickets 
-      current_user.save
+      attraction = Attraction.find(params[:id])
+      ride = Ride.create(user_id: current_user.id, attraction_id: attraction.id)
+      ride.take_ride
+      # current_user.tickets -= attraction.tickets 
+      # current_user.save
       redirect_to user_path(current_user)
    end 
 end
