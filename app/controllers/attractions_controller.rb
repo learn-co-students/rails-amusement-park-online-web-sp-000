692bc0b1ced18e7396dc67f96a76291c3c46db63
CoderJay06 <jaycru06@yahoo.com>
2020-08-01 23:19:39 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -13,8 +13,16 @@ class AttractionsController < ApplicationController
       attraction = Attraction.find(params[:id])
       ride = Ride.create(user_id: current_user.id, attraction_id: attraction.id)
       ride.take_ride
+
       if !ride.user_height_not_tall_enough? && ride.user_tickets_are_enough
          flash[:notice] = "Thanks for riding the #{attraction.name}!"
+      elsif ride.user_tickets_and_height_not_enough? 
+         flash[:notice] = "You do not have enough tickets to ride the #{attraction.name}"
+         flash[:notice] = "You are not tall enough to ride the #{attraction.name}"
+      elsif ride.user_height_not_tall_enough?
+         flash[:notice] = "You are not tall enough to ride the #{attraction.name}"
+      elsif !ride.user_tickets_are_enough
+         flash[:notice] = "You do not have enough tickets to ride the #{attraction.name}"
       end 
       # current_user.tickets -= attraction.tickets 
       # current_user.save
