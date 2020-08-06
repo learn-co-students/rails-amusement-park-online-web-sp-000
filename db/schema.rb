844b0215ab531068d6dba43d3ebf5866933ebd0a
ksummerill <kaitlynsummerill@gmail.com>
2020-08-05 17:07:38 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_152411) do
+ActiveRecord::Schema.define(version: 2020_08_06_000610) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_152411) do
     t.integer "tickets"
     t.integer "height"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
