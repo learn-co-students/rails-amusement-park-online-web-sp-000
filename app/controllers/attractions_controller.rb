4ace545abf9a724e83f7d425e9dd4980ec0ec682
ksummerill <kaitlynsummerill@gmail.com>
2020-08-07 16:53:47 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,7 +7,15 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.new
   end
 
+  def create
+    @attraction = Attraction.create(attraction_params)
+
   def show
-    @attraction = Attraction.find_by(:id => params[:id])
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 end
