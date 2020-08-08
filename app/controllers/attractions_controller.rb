1a95f2a5007692b50444573c9559af3f4fb4563f
ksummerill <kaitlynsummerill@gmail.com>
2020-08-07 17:14:33 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,4 +1,6 @@
 class AttractionsController < ApplicationController
+  before_action :find_attraction, only: [:show, :edit, :update]
+
   def index
     @attractions = Attraction.all
   end
@@ -9,11 +11,21 @@ class AttractionsController < ApplicationController
 
   def create
     @attraction = Attraction.create(attraction_params)
+    if @attraction
+      redirect_to attraction_path(@attraction)
+    else
+      render :new
+    end
+  end
 
   def show
   end
 
   private
+
+  def find_attraction
+    @attraction = Attraction.find_by(id: params[:id])
+  end
 
   def attraction_params
     params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
