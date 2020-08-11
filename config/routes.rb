379ad5e3edcaf1c0a61ab9b58fcdba0f4c5f8307
ksummerill <kaitlynsummerill@gmail.com>
2020-08-10 17:59:25 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rides/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :rides
