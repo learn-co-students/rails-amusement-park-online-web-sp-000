9b60eccc88c4d9e0cbf7efe388c63ea928760f67
ksummerill <kaitlynsummerill@gmail.com>
2020-08-11 08:45:19 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   resources :attractions
 
   root 'welcome#home'
-  get '/rides/create' => 'rides#create'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/logout' => 'sessions#destroy'
+  get '/rides/create' => 'rides#create'
   post '/rides' => 'rides#create'
 end
