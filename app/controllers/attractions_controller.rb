cbba5bb1103ce66211cc503e035e48c63b3eff35
ksummerill <kaitlynsummerill@gmail.com>
2020-08-10 18:54:29 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -23,6 +23,7 @@ class AttractionsController < ApplicationController
   end
 
   def edit
+    @attraction = Attraction.find(params[:id])
   end
 
   private
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,10 +1,12 @@
 class RidesController < ApplicationController
-  before_action :require_login
-  
+  before_action :logged_in?
+
   def create
     ride = Ride.create(user_id: current_user.id, attraction_id: params[:attraction_id])
 
     if ride
+      message = ride.take_ride
+      flash[:notice] = message
       redirect_to user_path(ride.user)
     else
       redirect_to attraction_path(ride.attraction)
