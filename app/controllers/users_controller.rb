be2333060a937d15124ddc23306df0d5517249d8
ksummerill <kaitlynsummerill@gmail.com>
2020-08-11 07:30:31 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+     if @user.save
+       @user.update(user_params)
+       redirect_to user_path
+     else
+       render :edit
+     end
+  end
+
   def show
     @user = User.find(params[:id])
     @message = params[:message] if params[:message]
