c9efc7ad3bb2f94d5ec9fb11ff6f8bc42a360d9a
chienleow <chienleow1@gmail.com>
2020-08-26 15:41:47 -0400

    Done.

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,16 @@
+class SessionController < ApplicationController
+    def signin
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render 'signin'
+        end
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
-  get 'signin', to: 'session#signin' 
+
+  get 'signin', to: 'session#signin'
+  post 'signin', to: 'session#create'
+  
   resources :users
 end
