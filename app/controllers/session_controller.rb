b5c5c11d6dc181af3c207929dac23748c0cf8c17
chienleow <chienleow1@gmail.com>
2020-08-27 00:27:47 -0400

    Done.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,8 +4,8 @@ class SessionController < ApplicationController
     end
 
     def create
-        @user = User.find_by(name: params[:user][:name])
-        if @user && @user.authenticate(params[:user][:password])
+        if @user = User.find_by(name: params[:user][:name])
+        # if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
@@ -13,4 +13,9 @@ class SessionController < ApplicationController
         end
     end
 
+    def destroy
+        # session.delete(:user_id)
+        reset_session
+        redirect_to '/'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#home'
+  # root to: 'application#home'
 
   get 'signin', to: 'session#new'
   post 'signin', to: 'session#create'
+  
+  get 'logout', to: 'session#destroy'
   
   resources :users
 end
