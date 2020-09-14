3f20fb35d064b8a216ccfc1e40e91e86eedea128
chienleow <chienleow1@gmail.com>
2020-09-14 19:12:46 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,6 +1,22 @@
 class AttractionsController < ApplicationController
+    def new
+        @attraction = Attraction.new
+    end
+
     def create
         @attraction = Attraction.new(attraction_params)
+        @attraction.save
+        redirect_to attraction_path(@attraction)
+    end
+
+    def edit
+        @attraction = Attraction.find(params[:id])
+    end
+
+    def update
+        @attraction = Attraction.find(params[:id])
+        @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
     end
 
     def index
