4e034c67da49c4a45c166719d93cc737fa272952
chienleow <chienleow1@gmail.com>
2020-09-14 19:24:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'static#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: 'application#home'
 
   get 'signin', to: 'session#new'
   post 'signin', to: 'session#create'
