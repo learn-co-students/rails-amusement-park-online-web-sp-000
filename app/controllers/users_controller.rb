baa68d25fb7ab0ce601865fae7b75245c01c0429
clincoln12 <chris.lincoln12@gmail.com>
2020-10-05 18:01:29 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+        log_user_in(@user)
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :admin, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#home"
+
+  resources :users, only: [:show, :new, :create]
+  resources :attractions
+  resources :rides, only: [:create]
+
+  get "/signin", to: "sessions#new", as: "login"
+  post "/signin", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
