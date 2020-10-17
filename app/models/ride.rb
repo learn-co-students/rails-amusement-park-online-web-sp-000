e6ecbde7d8b7deb002a680e3140c63ac17b860b0
Yumacorn <ReachRyanMui@gmail.com>
2020-10-16 21:02:24 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,13 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        if user.tickets < attraction.tickets
+            "Sorry. You do not have enough tickets to ride the Roller Coaster."
+        elsif user.height < attraction.height
+            "Sorry. You are not tall enough to ride the #{attraction.name}."
+        end
+
+    end
 end
