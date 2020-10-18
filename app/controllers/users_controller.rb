2dd302f5161967f2b21037e4641ee0b2f3918565
zacharytq <zacharytq@gmail.com>
2020-10-18 13:41:04 -0400

    first couple of tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            render :new
+        end            
     end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "static#home"
   resources :users
   resources :attractions
+  get "/signup" => 'users#new'
   get '/signin' => 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
