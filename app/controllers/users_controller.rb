48d24069f09b2d2787ef9cc47775180a9a2e513b
Yumacorn <ReachRyanMui@gmail.com>
2020-10-18 17:22:16 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,18 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
     def new
         @user = User.new
     end
 
-    def create
-        binding.pry
-        @user = User.create(user_params)
-        binding.pry
+    def index
+    end
 
+    def create
+        @user = User.create(user_params)
         redirect_to users_path(@user)
     end
 
     def show
-        binding.pry
-
         @user = User.find(user_params)
-        binding.pry
     end
 
 
