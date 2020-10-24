c136ca84a8b4313356572647cbaaccd7832d3b7d
Taishawnk <Taishawnking1@yahoo.com>
2020-10-24 00:11:34 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+  
+  def home 
+  end
+
+  private
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to view this page"
+      redirect_to '/'
+    end
+  end
+  def logged_in?
+    !!current_user
+  end
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
