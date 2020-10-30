cf5eca532a9e62cafdc9fe8a3c8a31dc27170032
blumenthaler <blumenthaleralex@gmail.com>
2020-10-30 18:35:32 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,31 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        user = self.user
+        attraction = self.attraction
+        too_short = "You are not tall enough to ride the #{attraction.name}."
+        need_tix = "You do not have enough tickets to ride the #{attraction.name}."
+        errors = ["Sorry."]
+    
+        if user.tickets < attraction.tickets && user.height < attraction.min_height
+            errors << need_tix
+            errors << too_short
+            error_msg = errors.join(" ")
+        elsif user.tickets < attraction.tickets
+            errors << need_tix
+            error_msg = errors.join(" ")
+        elsif user.height < attraction.min_height
+            errors << too_short
+            error_msg = errors.join(" ")
+        else
+            user.update(
+                tickets: user.tickets -= attraction.tickets,
+                nausea: user.nausea += attraction.nausea_rating,
+                happiness: user.happiness += attraction.happiness_rating
+            )
+            msg = "Thanks for riding the #{attraction.name}!"
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :attractions
+  resources :rides
+
+  
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  root "application#home"
+  get '/logout', to: "sessions#destroy"
 end
