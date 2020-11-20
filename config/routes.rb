c45e190c291311bb098f030702b469d00d40074a
Joseph Perez <hello@josephdanielperez.com>
2020-11-20 12:47:29 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users
+
+  resources :attractions
+
+  namespace :admin do
+    resources :attractions
+  end
+
+#  resources :rides
+  post '/rides/new' => 'rides#new'
 end
