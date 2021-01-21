17e7792ee6770e2d6e438dcda47d62a4fbbb038e
Catherine Gloria <catherinegloria@gmail.com>
2021-01-21 14:47:33 -0800

    Added form_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    session[:name]
+  end
+
+  #def require_logged_in
+  #  redirect_to '/login' unless current_user
+  #end
 end
