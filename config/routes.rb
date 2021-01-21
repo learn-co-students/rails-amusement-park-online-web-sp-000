49aef66e84efda97731c9d4bfd4f542c0384d2e0
Catherine Gloria <catherinegloria@gmail.com>
2021-01-21 14:58:43 -0800

    Home page edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
 
-  get '/signin', to: 'session#new'#, as: 'signin'
+  get '/signin', to: 'session#new', as: 'signin'
   post '/session', to: 'session#create' #, as: 'session'
   delete '/session/', to: 'session#destroy'
 
