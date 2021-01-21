65a58391f0e3b479c801b1524fdba00c2ec5ec8c
Catherine Gloria <catherinegloria@gmail.com>
2021-01-21 13:51:36 -0800

    Passed Models specs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,47 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    if user_has_enough_tickets && user_is_tall_enough
+      start_ride
+    elsif !user_has_enough_tickets && user_is_tall_enough
+      "Sorry. " + not_enough_tickets
+    elsif user_has_enough_tickets && !user_is_tall_enough
+      "Sorry. " + not_tall_enough
+    elsif !user_has_enough_tickets && !user_is_tall_enough
+      "Sorry. " + not_enough_tickets + " " + not_tall_enough
+    end
+  end
+
+  private
+
+  def start_ride
+    new_ticket_count = self.user.tickets - self.attraction.tickets
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+
+    self.user.update(
+      tickets: new_ticket_count,
+      nausea: new_nausea,
+      happiness: new_happiness
+    )
+  end
+
+  def user_has_enough_tickets
+    return true if self.user.tickets >= self.attraction.tickets
+  end
+
+  def user_is_tall_enough
+    return true if self.user.height >= self.attraction.min_height
+  end
+
+  def not_enough_tickets
+    "You do not have enough tickets to ride the #{self.attraction.name}."
+  end
+
+  def not_tall_enough
+    "You are not tall enough to ride the #{self.attraction.name}."
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_045631) do
+ActiveRecord::Schema.define(version: 2021_01_21_212004) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
