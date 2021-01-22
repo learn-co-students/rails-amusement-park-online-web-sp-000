21f6ceb0531a07e355545309e60231412e91ac6e
Catherine Gloria <catherinegloria@gmail.com>
2021-01-21 20:42:38 -0800

    Complete

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,12 +9,12 @@ class SessionController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to new_user_path(@user)
+      redirect_to "new"
     end
   end
 
     def destroy
-      session.delete :name
+      session.delete :user_id
       redirect_to root_path
     end
 
