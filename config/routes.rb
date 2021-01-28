745e1463508d5fb8a7186b31398b498f5453ed5a
Chandra Lama <iamme24cl@gmail.com>
2021-01-28 00:51:22 -0800

    user new and create working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+  resources :users, only: [:new, :create, :index, :show]
+  resources :attractions, except: [:destroy]
+  resources :rides, only: [:new, :create]
 end
