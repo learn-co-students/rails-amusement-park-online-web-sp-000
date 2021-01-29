5507b20da4ab0d6b2cf5febacc0d7dea37f79605
Jenise Ruehle <jeniseruehle@gmail.com>
2021-01-29 11:12:31 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :require_logged_in
+  before_action :require_logged_in, except: [:new, :create, :index]
   helper_method :current_user
 
   private
   
   def require_logged_in
-    redirect_to '/' unless logged_in?
+    redirect_to root_path unless logged_in?
   end
 
   def logged_in?
