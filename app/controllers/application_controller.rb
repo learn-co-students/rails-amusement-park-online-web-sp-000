de4b992ccbeb93ce89c55bf84d5794165a2cd365
Jenise Ruehle <jeniseruehle@gmail.com>
2021-01-28 18:31:37 -0600

    Debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_logged_in
+  helper_method :current_user
+
+  private
+  
+  def require_logged_in
+    redirect_to '/' unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static#home"
+  resources :users
+  resources :attractions
+  resources :rides, only: [:create]
+
+  get '/signup' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 end
