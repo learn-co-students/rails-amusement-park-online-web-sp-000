1452cfe86feded95ca873ac7e91bf0243bb639b3
catprice444 <catprice444@gmail.com>
2021-02-27 17:03:23 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  resources :attractions
+  resources :rides
+  resources :users
+  root 'static#home'
+  
+  
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
