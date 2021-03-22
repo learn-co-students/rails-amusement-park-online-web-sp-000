f6f52d09ec9f64826b3cfdbb8fa30d7f04fd3c73
bailaoye <jeffbai996@gmail.com>
2021-03-22 19:01:51 +0800

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:show]
+
+  def new
+  end
+
+  def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :admin, :password)
+  end
+end
