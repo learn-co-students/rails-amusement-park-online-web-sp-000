93531922bfde6bfa90df744d1d0bd90c08bce912
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-07 13:37:06 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new]
+  get '/signin', to: 'session#login'
+  post '/signout', to: 'session#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
