48a66a90c007c9484bbb7fb62b62865841bbef77
danny galimi <dannygalimi13@aim.com>
2021-04-08 22:41:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/new', to: 'users#new', as: 'new_user'
+  root 'static#home'
+  get '/users', to:'users#index', as: 'users'
+  post '/users', to:'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/attractions', to: 'attractions#index', as: 'attractions'
+  get '/signin', to: 'session#new', as: 'signin'
+  post '/session', to: 'session#create', as: 'session'
 end
