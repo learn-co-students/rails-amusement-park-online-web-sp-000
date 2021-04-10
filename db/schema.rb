01d0778cedff8e36219db1a49d878d0c3b3f117b
Amanda Rodgers <aarodgerswork@gmail.com>
2021-04-10 11:33:48 -0600

    adds users views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_171418) do
+ActiveRecord::Schema.define(version: 2021_04_10_172545) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_04_10_171418) do
     t.integer "min_height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attraction_id"], name: "index_rides_on_attraction_id"
+    t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
