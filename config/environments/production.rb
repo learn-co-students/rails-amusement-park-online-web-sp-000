c73285a64e0467e29bbce9f379dabcdad0ed6e55
Allison <allimacq@gmail.com>
2021-05-06 17:17:55 -0400

    Done.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
+  config.active_record.sqlite3.represent_boolean_as_integer = true
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :attractions, only: [:new, :create, :edit, :show, :update]
+  resources :users, only: [:new, :create, :show, :update]
+  get '/', to: "users#index"
+  get '/signin', to: "users#signin"
+  post '/signin', to: "users#verify_signin"
+  root "users#index"
+  delete '/logout', to: "users#destroy"
+
+  get '/attractions', to: "attractions#index"
+
 end
