d3271ae98faacbe3e4e1a41d5feb4e07a54a68d4
jrrohrer <jessie.rohrer@gmail.com>
2021-05-11 20:18:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authorized_user
+  skip_before_action :authorized_user, only: [:index]
+  helper_method :current_user
+
+  def index
+  end
+
+  private
+
+  def authorized_user
+    redirect_to '/' unless signed_in
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def signed_in
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
+
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+
+  get '/attractions', to: 'attractions#index', as: 'attractions'
+  get '/attractions/new', to: 'attractions#new', as: 'new_attraction'
+  get '/attractions/:id', to: 'attractions#show', as: 'attraction'
+  get '/attractions/:id/edit', to: 'attractions#edit', as: 'edit_attraction'
+  patch '/attractions/:id', to: 'attractions#update'
+  post '/attractions', to: 'attractions#create'
+
+  post '/rides', to: 'rides#create', as: 'rides'
+    
+  get '/signin', to: 'sessions#new', as: 'signin'
+  post '/session', to: 'sessions#create', as: 'session'
+  delete '/session', to: 'sessions#destroy'
+  
 end
