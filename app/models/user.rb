e0a3c295a3a8a4dfa75d2b30e0ebb39d6f0ed3b6
Christerle Nounoute <christerlesn@gmail.com>
2021-05-23 23:22:13 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
+  has_many :rides
+  has_many :attractions, through: :rides
   has_secure_password
+
+  def mood
+    if self.nausea > self.happiness
+      return "sad"
+    elsif self.nausea < self.happiness
+      return "happy"
+    else
+    end
+  end
+
 end
