927ff9847b7a93350186a5a5d271d923cdb9e390
Christerle Nounoute <christerlesn@gmail.com>
2021-05-23 21:24:28 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    # raise params.inspect
     @user = User.create(user_params)
     session[:user_id] = @user.id
     redirect_to "/users/#{@user.id}"
   end
 
   def signin
-    @users = User.all
+    @user = User.find_by(:id => params[:id])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
+    else
+    end
   end
 
   def show
     @user = User.find_by(:id => params[:id])
   end
-  #
-  # def index
-  #   @users = User.all
-  # end
 
 
   private
