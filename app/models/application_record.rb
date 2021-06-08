80708f73d3b496322d8f35f51d6cdccf6519f821
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-06-08 16:01:34 -0500

    Done.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
+  # if you need some logic that will work for all or some classes(models) you would usually put that inside ApplicationRecord class so you follow the DRY pattern
   self.abstract_class = true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    validates :name, uniqueness: true, length: { in: 6..16 }
+    validates :password, presence: true, length: { in: 6..12 }
+    validates :happiness, length: { in: 1..10 }
+    validates :nausea, length: { in: 1..10 }
+    validates :height, length: { in: 16..75 }
+    validates :tickets, length: { in: 0..500 }
+    validates :admin # boolean defaults to false
+    has_many :rides
+    has_many :attractions, through: :rides
+    has_secure_password
 end
