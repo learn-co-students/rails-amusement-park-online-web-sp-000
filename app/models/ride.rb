1da8be0d0ad18f4366f3883da9311815098bd843
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-11 13:12:08 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,51 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  #"belongs to one attraction"
+  belongs_to :user
+
+  def take_ride
+    #set conditions so can give error message accordingly
+    #makes sense to have a helper method for that so this isn't cluttered
+
+    enough_tickets = meet_requirements[0]
+    tall_enough = meet_requirements[1]
+
+    if enough_tickets && tall_enough
+      begin_ride #move ride logic to another method
+    elsif tall_enough && !enough_tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif enough_tickets && !tall_enough
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    end
+  end
+
+  def begin_ride
+    # taking a ride just means to update the user attributes accordingly
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+    #"has a method 'take_ride' that updates the user's happiness"
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    #"has a method 'take_ride' that updates the user's nausea"
+    new_tickets =  self.user.tickets - self.attraction.tickets
+    #"has a method 'take_ride' that updates ticket number"
+    self.user.update(
+      :happiness => new_happiness,
+      :nausea => new_nausea,
+      :tickets => new_tickets
+    )
+  end
+
+  #does the user meet the requirements to take the ride? can't just
+  #return single true or false because error message will differ depending on which requirments are not met
+
+  #"has a method 'take_ride' that accounts for the user not having enough tickets"
+  #"has a method 'take_ride' that accounts for the user not being tall enough"
+  #"has a method 'take_ride' that accounts for the user not being tall enough and not having enough tickets"
+  def meet_requirements
+    enough_tickets = (self.user.tickets >= self.attraction.tickets)
+    tall_enough = (self.user.height >= self.attraction.min_height)
+    [enough_tickets, tall_enough]
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates :password, presence: true, on: :create
+  validates :name, presence: true, uniqueness: true
+  validates :height, inclusion: {in: 1..100, message: "must be between 1 and 100"}, on: :create, :allow_blank => true
+  validates :happiness, inclusion: {in: 1..5, message: "must be between 1 and 5"}, on: :create, :allow_blank => true
+  validates :nausea, inclusion: {in: 1..5, message: "must be between 1 and 5"}, on: :create, :allow_blank => true
+  
+  def mood
+    self.nausea > self.happiness ? "sad" : "happy"
+  end
+
 end
