a25904d3b526ce6ac0570eff5515f88df4edb09b
JC <bonassin@gmail.com>
2021-06-14 17:58:43 -0500

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,5 @@
 class Attraction < ApplicationRecord
-end
+    has_many :rides
+    has_many :users, through: :rides
+    validates :name, :min_height, :nausea_rating, :happiness_rating, :tickets, presence: true
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
-end
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+    validates :password_digest, presence: true
+  
+    def mood
+      unless admin
+        happiness > nausea ? "happy" : "sad"
+      end
+    end
+  
+  end
