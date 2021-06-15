f57ec5f720417632ed3d2a5bde281c44700a9b62
GoldenPavilion <rcorystine@gmail.com>
2021-06-15 14:32:14 -0400

    update for review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#home'
+  
+  resources :users, only: [:new, :create, :show]
+  
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
+
+  #resources :attractions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
