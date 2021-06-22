a1819f256893a30bcf300d1e4f37d03edf8cbf61
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-06-22 17:10:35 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,19 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
+        if @user.valid?
+            @user.save
             session[:user_id] = @user.id
-            redirect_to root_path
+            redirect_to controller: 'users', action: 'show'
         else
             redirect_to controller: 'users', action: 'create'
         end
     end
 
-    def root
-
+    def show
+        # byebug
+        @user = User.find(params[:id])
+        render template: 'users/show'
     end
 
     private
