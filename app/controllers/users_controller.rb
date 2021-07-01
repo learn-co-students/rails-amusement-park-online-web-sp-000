f25e60aae14c6f6ccad54c520de49f9cd14b2673
McKayla Lankau <lankaukk@yahoo.com>
2021-07-01 16:47:09 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    if (user = User.create(user_params))
-      session[:user_id] = user.id
+    @user = User.create(user_params)
+    if @user.save
+      @user.id = session[:user_id]
       redirect_to user_path(user)
     else
       render 'new'
     end
   end
-
+      
   def show
     @user = User.find_by(id: params[:id])
   end
